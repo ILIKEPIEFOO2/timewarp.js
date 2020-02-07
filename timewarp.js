c96885ac16fed3e21b64d05936546f5b23611d82
ILIKEPIEFOO2 <37884580+ILIKEPIEFOO2@users.noreply.github.com>
2020-02-06 16:19:37 -0800

    Added motion saving.

--- timewarp.js
+++ timewarp.js
@@ -2,11 +2,12 @@ var lastPoint;
 events.listen("player.tick",function(event){
 	if(event.player.ticksExisted%100==0){
 		var player=event.player;
-		var current={x:player.x,y:player.y,z:player.z,pitch:player.pitch,yaw:player.yaw};
+		var current={x:player.x,y:player.y,z:player.z,pitch:player.pitch,yaw:player.yaw,mx:player.motionX,my:player.motionY,mz:player.motionZ};
 		if(!lastPoint){
 			lastPoint=current;
 		}else{
-			event.player.setPositionAndRotation(lastPoint.x,lastPoint.y,lastPoint.z,lastPoint.yaw,lastPoint.pitch);
+			player.setPositionAndRotation(lastPoint.x,lastPoint.y,lastPoint.z,lastPoint.yaw,lastPoint.pitch);
+			player.setMotion(lastPoint.mx,lastPoint.my,lastPoint.mz);
 			lastPoint=current;
 		}
 	}
